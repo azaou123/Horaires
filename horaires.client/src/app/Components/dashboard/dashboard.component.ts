f80a3b7e2969fa62e65f153e75ce8b5d30f87e93
Azaou123 <azawbadr@gmail.com>
2024-05-13 22:07:24 +0100

    set up template for client side

---
+++ horaires.client/src/app/Components/dashboard/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [
+    './dashboard.component.css',
+    '../../../assets/css/bootstrap.min.css',
+    '../../../assets/style.css',
+    '../../../assets/css/responsive.css',
+    '../../../assets/css/color_2.css',
+    '../../../assets/css/bootstrap-select.css',
+    '../../../assets/css/perfect-scrollbar.css',
+    '../../../assets/css/custom.css'
+  ]
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
